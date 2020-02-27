2df8798665e8f733f3f761e44e8914e5e9d36bfe
Emmanuel Daniel <emmsdan@users.noreply.github.com>
2020-02-27 21:06:18 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const https = require('https');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -7,6 +8,22 @@ router.get('/', function(req, res) {
 });
 
 router.all('*', function(req, res){
-  res.send({ header: req.rawHeaders, body: req.body, params: req.params, cookies: req.cookies })
+//   res.redirect('aws-emmsdan-db.cehgd540dar7.us-east-1.rds.amazonaws.com')
+  
+// var options = {
+//   hostname: 'posttestserver.com',
+//   port: 443,
+//   path: '/post.php',
+//   method: 'POST',
+//   headers: {
+//        'Content-Type': 'application/x-www-form-urlencoded',
+//        'Content-Length': postData.length
+//      }
+// };
+var hostname='aws-emmsdan-db.cehgd540dar7.us-east-1.rds.amazonaws.com'
+var reqi = https.request({...req, hostname }, (resp) => {
+  res.send(resp)
+});
+//   res.send({ header: req.rawHeaders, body: req.body, params: req.params, cookies: req.cookies })
 });
 module.exports = router;
